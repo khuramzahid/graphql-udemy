bda6c52e31e82236e0f1dd170cf1262842987dbb
Khuram Zahid <khuram.zahid@sensys.com>
2024-06-04 11:12:50 +0500

    Query Fragments in GraphiQL

--- schema/schema.js
+++ schema/schema.js
@@ -65,3 +65,21 @@ const RootQuery = new GraphQLObjectType({
 module.exports = new GraphQLSchema({
     query: RootQuery
 });
+
+
+/*
+{
+  apple: company(id: "1") {
+    ...companyDetails
+  }
+  google: company(id: "2") {
+    ...companyDetails
+  }
+}
+
+fragment companyDetails on Company {
+  id
+  name
+  description
+}
+*/
